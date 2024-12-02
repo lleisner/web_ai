d89c6659e074da613d98d6f14637fb0af8f1595c
lleisner <82710557+lleisner@users.noreply.github.com>
2024-12-02 13:52:36 +0100

    api key added correctly

--- task1-template/app.py
+++ task1-template/app.py
@@ -1,12 +1,11 @@
 import streamlit as st
-#from utils import load_api_key
 from game_app import GameApp
 
 @st.cache_resource
 def intialize_app(_api_key):
     return GameApp(_api_key)
 
-#api_key = load_api_key()
-api_key = "Your api key here"
+api_key = st.secrets["openai_api_key"]
+
 app = intialize_app(api_key)
 app.run()
